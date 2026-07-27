2b8d64d16f47f14ca9372d3f9405cd43c1984285
n-saraseka <nicksaraseka43@gmail.com>
2026-07-27 23:13:17 +0300

    And I also missed UserRepository

--- DbService/Repositories/UserRepository.cs
+++ DbService/Repositories/UserRepository.cs
@@ -3,7 +3,7 @@ using OsuScoreStats.DbService.Repositories.Interfaces;
 using OsuScoreStats.DbService.Entities;
 namespace OsuScoreStats.DbService.Repositories;
 
-public class UserRepository(DbContext db) : BaseRepository<User, int>(db), IUserRepository
+public class UserRepository(ScoreDataContext db) : BaseRepository<User, int>(db), IUserRepository
 { 
     // Only exists to keep things the same as other repositories for now.
 }

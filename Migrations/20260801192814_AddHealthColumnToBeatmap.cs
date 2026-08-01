c3ce60d9c29093b34fb5c6029e903214287326d6
n-saraseka <nicksaraseka43@gmail.com>
2026-08-01 23:37:39 +0300

    Commit missing migrationf iles

---
+++ Migrations/20260801192814_AddHealthColumnToBeatmap.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OsuScoreStats.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddHealthColumnToBeatmap : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "drain_length",
+                table: "beatmaps",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+
+            migrationBuilder.AddColumn<float>(
+                name: "health",
+                table: "beatmaps",
+                type: "real",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "health",
+                table: "beatmaps");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "drain_length",
+                table: "beatmaps",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+    }
+}

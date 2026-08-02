12c245f31f058c2fb1eba8b00a7353c305a3b46a
n-saraseka <nicksaraseka43@gmail.com>
2026-08-03 00:53:26 +0300

    Migrations

---
+++ Migrations/20260802204357_AddUserIdToBeatmaps.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OsuScoreStats.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserIdToBeatmaps : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "preview_url",
+                table: "beatmapsets");
+
+            migrationBuilder.AddColumn<int>(
+                name: "user_id",
+                table: "beatmaps",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_beatmaps_user_id",
+                table: "beatmaps",
+                column: "user_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_beatmaps_users_user_id",
+                table: "beatmaps",
+                column: "user_id",
+                principalTable: "users",
+                principalColumn: "id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_beatmaps_users_user_id",
+                table: "beatmaps");
+
+            migrationBuilder.DropIndex(
+                name: "ix_beatmaps_user_id",
+                table: "beatmaps");
+
+            migrationBuilder.DropColumn(
+                name: "user_id",
+                table: "beatmaps");
+
+            migrationBuilder.AddColumn<string>(
+                name: "preview_url",
+                table: "beatmapsets",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}

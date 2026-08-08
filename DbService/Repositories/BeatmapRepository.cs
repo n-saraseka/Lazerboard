9e4215fd6a7e3e7197c3c8838ed5f612ff36ff26
n-saraseka <nicksaraseka43@gmail.com>
2026-08-08 20:45:14 +0300

    Group beatmapset difficulties by mode

--- DbService/Repositories/BeatmapRepository.cs
+++ DbService/Repositories/BeatmapRepository.cs
@@ -8,7 +8,8 @@ public class BeatmapRepository(ScoreDataContext db) : BaseRepository<Beatmap, in
     public Task<List<Beatmap>> GetByBeatmapsetIdAsync(int beatmapsetId, CancellationToken ct) =>
         Set
             .Where(b => b.BeatmapsetId == beatmapsetId)
-            .OrderBy(b => b.Difficulty)
+            .OrderBy(b => b.Mode)
+            .ThenBy(b => b.Difficulty)
             .Include(b => b.Beatmapset)
             .ThenInclude(bs => bs.User)
             .ToListAsync(ct);

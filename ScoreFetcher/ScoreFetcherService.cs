70531e3148c738d02d65c7ac4a865d404531a185
n-saraseka <nicksaraseka43@gmail.com>
2026-08-11 02:19:38 +0300

    Add CursorString setting to appsettings.json

--- ScoreFetcher/ScoreFetcherService.cs
+++ ScoreFetcher/ScoreFetcherService.cs
@@ -23,7 +23,8 @@ public class ScoreFetcherService : BackgroundService
         var config = scope.ServiceProvider.GetRequiredService<IConfiguration>();
         
         _apiInterval = double.Parse(config["OsuApiInterval"]);
-        
+
+        _cursor = config["CursorString"];
         _firehosePath = config["FirehosePath"];
         _shouldUseFirehose = File.Exists(_firehosePath);
 
@@ -64,7 +65,7 @@ public class ScoreFetcherService : BackgroundService
         if (beatmapsets.Count == 0)
         {
             _logger.Log(LogLevel.Information, "No beatmapsets found.");
-            if (_repeatExponent >= 4)
+            if (_repeatExponent > 4)
             {
                 _logger.Log(LogLevel.Information, "No beatmapsets found after {seconds} seconds. Switching to using the firehose", 
                     _apiInterval * Math.Pow(2, _repeatExponent));

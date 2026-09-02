063bca32e8b66d4531b0e5abcf485f359da6da45
n-saraseka <nicksaraseka43@gmail.com>
2026-09-02 17:08:50 +0300

    Add Quartz service to Lazerboard

--- Backend/Lazerboard/Program.cs
+++ Backend/Lazerboard/Program.cs
@@ -6,6 +6,8 @@ using Lazerboard.Data.Database;
 using Lazerboard.Data.Database.Repositories;
 using Lazerboard.Data.Database.Repositories.Interfaces;
 using Lazerboard.Data.OsuEntities.Enums;
+using Lazerboard.Jobs;
+using Quartz;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -84,6 +86,19 @@ builder.Services.AddRateLimiter(options =>
         await context.HttpContext.Response.WriteAsync("Too many requests. Please try again later.", cancellationToken);
     };
 });
+
+// Quartz jobs
+builder.Services.AddQuartz(q =>
+{
+    q.ScheduleJob<GetLatestScannedBeatmapJob>(trigger => trigger
+        .WithIdentity("Latest scanned beatmap job")
+        .WithSchedule(SimpleScheduleBuilder.Create()
+            .WithIntervalInMinutes(5)
+            .RepeatForever())
+    );
+});
+
+builder.Services.AddQuartzHostedService(q => q.WaitForJobsToComplete = true);
 
 var app = builder.Build();
 

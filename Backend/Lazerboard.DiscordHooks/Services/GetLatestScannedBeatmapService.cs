6bd73cbd697d87fafc03a5cf8cb5c8dbd7cefd7e
n-saraseka <nicksaraseka43@gmail.com>
2026-09-03 22:54:18 +0300

    Use beatmapset data instead of beatmap data for the beatmap scans hook

--- Backend/Lazerboard.DiscordHooks/Services/GetLatestScannedBeatmapService.cs
+++ Backend/Lazerboard.DiscordHooks/Services/GetLatestScannedBeatmapService.cs
@@ -2,6 +2,7 @@ using Discord;
 using Discord.Webhook;
 using Lazerboard.Data.Database.Entities;
 using Lazerboard.Data.Database.Repositories.Interfaces;
+using Lazerboard.Data.OsuEntities.Enums;
 using Lazerboard.DiscordHooks.Utils;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -38,13 +39,17 @@ public class GetLatestScannedBeatmapService : BackgroundService
         {
             try
             {
-                var beatmap = await GetBeatmapDataAsync(cancellationToken);
-                if (beatmap != null)
+                var beatmaps = await GetBeatmapsDataAsync(cancellationToken);
+                if (beatmaps.Count > 0)
                 {
-                    var embed = BuildBeatmapEmbed(beatmap);
+                    var embed = BuildBeatmapsetEmbed(beatmaps);
+                    
                     using var client = new DiscordWebhookClient(_webhookUrl);
-                    _logger.Log(LogLevel.Information, "Most recent scanned beatmap ID: {beatmapId}", beatmap.Id);
-                    await client.SendMessageAsync("Most recent scanned beatmap:", false, [embed]);
+                    
+                    var beatmapsetId = beatmaps.First().BeatmapsetId;
+                    _logger.Log(LogLevel.Information, "Most recent scanned beatmapset ID: {beatmapId}", beatmapsetId);
+                    
+                    await client.SendMessageAsync("Most recent scanned beatmapset:", false, [embed]);
                 }
             }
             catch (Exception ex)
@@ -56,42 +61,97 @@ public class GetLatestScannedBeatmapService : BackgroundService
             await Task.Delay(_updateInterval, cancellationToken);
         }
     }
-
-    private async Task<Beatmap?> GetBeatmapDataAsync(CancellationToken cancellationToken)
+    
+    /// <summary>
+    /// Get <see cref="Beatmap"/> data from the most recent <see cref="Beatmapset"/>
+    /// </summary>
+    /// <param name="cancellationToken">A <see cref="CancellationToken"/></param>
+    /// <returns>List of <see cref="Beatmap"/>s</returns>
+    private async Task<List<Beatmap>> GetBeatmapsDataAsync(CancellationToken cancellationToken)
     {
-        _logger.Log(LogLevel.Information, "Getting most recent scanned beatmap...");
+        _logger.Log(LogLevel.Information, "Getting most recent scanned beatmapset...");
         
         using var scope = _serviceProvider.CreateScope();
         
         var scoreRepository = scope.ServiceProvider.GetRequiredService<IScoreRepository>();
         var beatmapRepository = scope.ServiceProvider.GetRequiredService<IBeatmapRepository>();
         
-        var beatmapId = await scoreRepository.GetMaxBeatmapIdAsync(cancellationToken);
-        var beatmapData = await beatmapRepository.GetWithBeatmapsetDataAsync(beatmapId, cancellationToken);
+        var beatmapsetId = await scoreRepository.GetMaxBeatmapsetIdAsync(cancellationToken);
+        var beatmapsData = await beatmapRepository.GetByBeatmapsetIdAsync(beatmapsetId, cancellationToken);
         
-        return beatmapData;
+        return beatmapsData;
     }
 
-    private Embed BuildBeatmapEmbed(Beatmap beatmap)
-    {
-        var title = $"{beatmap.Beatmapset.Artist} - {beatmap.Beatmapset.Title} [{beatmap.DifficultyName}]";
-        
-        var mapsetBy = $"**Mapset by**: [{beatmap.Beatmapset.Creator}](https://osu.ppy.sh/users/{beatmap.Beatmapset.UserId})";
-        var mode = $"**Mode**: {EmbedUtils.GetModeText(beatmap.Mode)}";
-        var difficulty = $"**Difficulty**: {Math.Round(beatmap.Difficulty, 2)} :star:";
-        var beatmapStatus = $"**Status**: {EmbedUtils.GetStatusText(beatmap.Status)}";
+    /// <summary>
+    /// Get the main <see cref="Mode"/> from a list of <see cref="Beatmap"/>s
+    /// </summary>
+    /// <param name="beatmaps">The List of <see cref="Beatmap"/>s</param>
+    /// <returns>The main <see cref="Mode"/></returns>
+    private Mode GetMainMode(IList<Beatmap> beatmaps) =>
+        beatmaps
+            .OrderBy(b => b.Mode)
+            .GroupBy(b => b.Mode)
+            .Select(g => new { Mode = g.Key, Count = g.Count() })
+            .OrderByDescending(b => b.Count)
+            .ThenBy(b => b.Mode)
+            .First().Mode;
 
-        var imageUrl = $"https://assets.ppy.sh/beatmaps/{beatmap.Beatmapset.Id}/covers/cover@2x.jpg";
-        var thumbnailUrl = $"https://a.ppy.sh/{beatmap.Beatmapset.UserId}";
-        var beatmapUrl = $"https://osu.ppy.sh/b/{beatmap.Id}";
+    /// <summary>
+    /// Get <see cref="Mode"/>s string from a list of <see cref="Beatmap"/>s
+    /// </summary>
+    /// <param name="beatmaps">The List of <see cref="Beatmap"/>s</param>
+    /// <returns>The <see cref="Mode"/>s string</returns>
+    private string GetModesString(IList<Beatmap> beatmaps)
+    {
+        var modes = beatmaps
+            .Select(b => b.Mode)
+            .Distinct()
+            .ToList();
+        
+        var modeStrings = modes.Select(EmbedUtils.GetModeText).ToList();
+        return string.Join(", ", modeStrings);
+    }
+
+    /// <summary>
+    /// Get a list of <see cref="BeatmapStatus"/>es from a list of <see cref="Beatmap"/>s
+    /// </summary>
+    /// <param name="beatmaps">The List of <see cref="Beatmap"/>s</param>
+    /// <returns>The list of <see cref="BeatmapStatus"/>es</returns>
+    private string GetStatusesString(IList<Beatmap> beatmaps)
+    {
+        var statuses = beatmaps
+            .Select(b => b.Status)
+            .Distinct()
+            .ToList();
+        
+        var statusStrings = statuses.Select(EmbedUtils.GetStatusText).ToList();
+        return string.Join(", ", statusStrings);
+    }
+
+    private Embed BuildBeatmapsetEmbed(IList<Beatmap> beatmaps)
+    {
+        var firstBeatmap = beatmaps.First();
+        var title = $"{firstBeatmap.Beatmapset.Artist} - {firstBeatmap.Beatmapset.Title}";
+        
+        var mainMode = GetMainMode(beatmaps);
+        var beatmapsetModes = GetModesString(beatmaps);
+        var beatmapStatuses = GetStatusesString(beatmaps);
+        
+        var mapsetBy = $"**Mapset by**: [{firstBeatmap.Beatmapset.Creator}](https://osu.ppy.sh/users/{firstBeatmap.Beatmapset.UserId})";
+        var mode = $"**Beatmapset modes**: {beatmapsetModes}";
+        var beatmapStatus = $"**Beatmap statuses**: {beatmapStatuses}";
+
+        var imageUrl = $"https://assets.ppy.sh/beatmaps/{firstBeatmap.Beatmapset.Id}/covers/cover@2x.jpg";
+        var thumbnailUrl = $"https://a.ppy.sh/{firstBeatmap.Beatmapset.UserId}";
+        var beatmapUrl = $"https://osu.ppy.sh/beatmapsets/{firstBeatmap.Beatmapset.Id}";
         
         var timestamp = DateTimeOffset.UtcNow;
-        var color = EmbedUtils.GetModeColor(beatmap.Mode);
+        var color = EmbedUtils.GetModeColor(mainMode);
         
         var builder = new EmbedBuilder
         {
             Title = title,
-            Description = $"{mapsetBy}\n{mode}\n{difficulty}\n{beatmapStatus}",
+            Description = $"{mapsetBy}\n{mode}\n{beatmapStatus}",
             Color = color,
             ImageUrl = imageUrl,
             ThumbnailUrl = thumbnailUrl,

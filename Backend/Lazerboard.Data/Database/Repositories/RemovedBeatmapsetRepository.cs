f030e52407c8eee9aa18ecf303eb98e2ef219d07
n-saraseka <nicksaraseka43@gmail.com>
2026-09-11 11:24:49 +0300

    Add missing repository registration

--- Backend/Lazerboard.Data/Database/Repositories/RemovedBeatmapsetRepository.cs
+++ Backend/Lazerboard.Data/Database/Repositories/RemovedBeatmapsetRepository.cs
@@ -1,6 +1,9 @@
+using Lazerboard.Data.Database.Entities;
+using Lazerboard.Data.Database.Repositories.Interfaces;
+
 namespace Lazerboard.Data.Database.Repositories;
 
-public class RemovedBeatmapsetRepository
+public class RemovedBeatmapsetRepository(ScoreDataContext db) : BaseRepository<RemovedBeatmapset, int>(db), IRemovedBeatmapsetRepository
 {
     // Only exists to have things the same as other repositories for now.
 }

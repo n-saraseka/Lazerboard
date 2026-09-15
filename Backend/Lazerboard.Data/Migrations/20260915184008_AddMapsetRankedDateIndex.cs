f154c9196779ce7bc92a179db17bbff8834cd5cc
n-saraseka <nicksaraseka43@gmail.com>
2026-09-15 21:40:19 +0300

    Add mapset ranked date index

---
+++ Backend/Lazerboard.Data/Migrations/20260915184008_AddMapsetRankedDateIndex.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OsuScoreStats.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMapsetRankedDateIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "ix_beatmapsets_ranked_date",
+                table: "beatmapsets",
+                column: "ranked_date");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_beatmapsets_ranked_date",
+                table: "beatmapsets");
+        }
+    }
+}

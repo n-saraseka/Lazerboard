1beed239851586c3e9c43e0ef7ba14e2045c65a3
n-saraseka <nicksaraseka43@gmail.com>
2026-09-15 18:18:26 +0300

    Add missing migration files

---
+++ Backend/Lazerboard.Data/Migrations/20260915150732_AddBeatmapModeIndex.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OsuScoreStats.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBeatmapModeIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "ix_beatmaps_mode",
+                table: "beatmaps",
+                column: "mode");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_beatmaps_mode",
+                table: "beatmaps");
+        }
+    }
+}

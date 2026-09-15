b87e20dc87ce5ae16d178b058446325e6d269b05
n-saraseka <nicksaraseka43@gmail.com>
2026-09-15 17:50:23 +0300

    Increase delay between batches

--- Backend/Lazerboard.ScoreFetcher/BackgroundServices/BackpopulatorService.cs
+++ Backend/Lazerboard.ScoreFetcher/BackgroundServices/BackpopulatorService.cs
@@ -13,7 +13,7 @@ namespace Lazerboard.ScoreFetcher.BackgroundServices;
 public class BackpopulatorService(IServiceProvider serviceProvider, ILogger<BackpopulatorService> logger) : BackgroundService
 {
     private const int BatchSize = 75;
-    private const int DelayBetweenBatches = 50;
+    private const int DelayBetweenBatches = 500;
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {

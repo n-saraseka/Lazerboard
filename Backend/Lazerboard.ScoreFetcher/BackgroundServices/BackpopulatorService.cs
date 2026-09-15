57d5b6773e3c8c4cdddcd8fbf1487cc934a6ea02
n-saraseka <nicksaraseka43@gmail.com>
2026-09-15 18:40:35 +0300

    Optimize backpopulator

--- Backend/Lazerboard.ScoreFetcher/BackgroundServices/BackpopulatorService.cs
+++ Backend/Lazerboard.ScoreFetcher/BackgroundServices/BackpopulatorService.cs
@@ -12,7 +12,7 @@ namespace Lazerboard.ScoreFetcher.BackgroundServices;
 
 public class BackpopulatorService(IServiceProvider serviceProvider, ILogger<BackpopulatorService> logger) : BackgroundService
 {
-    private const int BatchSize = 50;
+    private const int BatchSize = 15;
     private const int DelayBetweenBatches = 500;
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -198,7 +198,7 @@ public class BackpopulatorService(IServiceProvider serviceProvider, ILogger<Back
         var updatedScores = await scoreRepo
             .GetDbContext()
             .Database
-            .ExecuteSqlAsync($"UPDATE scores s SET is_convert = (s.mode != v.mode) FROM unnest({batch.Select(kvp => kvp.Key)}, {batch.Select(kvp => kvp.Value)}) AS v(beatmap_id, mode) WHERE s.beatmap_id = v.beatmap_id AND s.is_convert IS NULL", 
+            .ExecuteSqlAsync($"UPDATE scores s SET is_convert = (s.mode != v.mode) FROM unnest({batch.Select(kvp => kvp.Key)}::int[], {batch.Select(kvp => kvp.Value)}::mode[]) AS v(beatmap_id, mode) WHERE s.beatmap_id = v.beatmap_id AND s.is_convert IS NULL", 
                 token);
         logger.Log(LogLevel.Information, "Added missing convert attributes to {scoreCount} scores", updatedScores);
         return true;

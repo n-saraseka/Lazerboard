89889d9768009f7c07feb8ad5b48c23503857751
n-saraseka <nicksaraseka43@gmail.com>
2026-09-20 19:53:42 +0300

    Coalesce user IDs for user scans service

--- Backend/Lazerboard.ScoreFetcher/BackgroundServices/ScanServices/UserScanService.cs
+++ Backend/Lazerboard.ScoreFetcher/BackgroundServices/ScanServices/UserScanService.cs
@@ -78,8 +78,9 @@ public class UserScanService(
     {
         using var scope = serviceProvider.CreateScope();
         var userRepository = scope.ServiceProvider.GetRequiredService<IUserRepository>();
+        var userId = _latestUserId ?? 0;
         return await userRepository.GetAll()
-            .Where(u => u.Id > _latestUserId)
+            .Where(u => u.Id > userId)
             .OrderBy(u => u.Id)
             .Take(BatchSize)
             .AsNoTracking()

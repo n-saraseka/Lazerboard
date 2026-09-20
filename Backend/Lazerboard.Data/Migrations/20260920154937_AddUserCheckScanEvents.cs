e2c68c93f1c296fb9bc55872a5e19dba2eef5109
n-saraseka <nicksaraseka43@gmail.com>
2026-09-20 18:49:58 +0300

    Add user check scan events

---
+++ Backend/Lazerboard.Data/Migrations/20260920154937_AddUserCheckScanEvents.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OsuScoreStats.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserCheckScanEvents : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:Enum:beatmap_status", "approved,graveyard,loved,pending,qualified,ranked,wip")
+                .Annotation("Npgsql:Enum:grade", "a,b,c,d,f,s,sh,x,xh")
+                .Annotation("Npgsql:Enum:mode", "fruits,mania,osu,taiko")
+                .Annotation("Npgsql:Enum:scan_event_type", "main_seeding_finished,main_seeding_started,rescan_finished,rescan_started,secondary_seeding_finished,secondary_seeding_started,user_check_finished,user_check_started,user_scan_finished,user_scan_started")
+                .Annotation("Npgsql:Enum:score_source", "leaderboard_scan,score_fetcher")
+                .OldAnnotation("Npgsql:Enum:beatmap_status", "approved,graveyard,loved,pending,qualified,ranked,wip")
+                .OldAnnotation("Npgsql:Enum:grade", "a,b,c,d,f,s,sh,x,xh")
+                .OldAnnotation("Npgsql:Enum:mode", "fruits,mania,osu,taiko")
+                .OldAnnotation("Npgsql:Enum:scan_event_type", "main_seeding_finished,main_seeding_started,rescan_finished,rescan_started,secondary_seeding_finished,secondary_seeding_started,user_scan_finished,user_scan_started")
+                .OldAnnotation("Npgsql:Enum:score_source", "leaderboard_scan,score_fetcher");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:Enum:beatmap_status", "approved,graveyard,loved,pending,qualified,ranked,wip")
+                .Annotation("Npgsql:Enum:grade", "a,b,c,d,f,s,sh,x,xh")
+                .Annotation("Npgsql:Enum:mode", "fruits,mania,osu,taiko")
+                .Annotation("Npgsql:Enum:scan_event_type", "main_seeding_finished,main_seeding_started,rescan_finished,rescan_started,secondary_seeding_finished,secondary_seeding_started,user_scan_finished,user_scan_started")
+                .Annotation("Npgsql:Enum:score_source", "leaderboard_scan,score_fetcher")
+                .OldAnnotation("Npgsql:Enum:beatmap_status", "approved,graveyard,loved,pending,qualified,ranked,wip")
+                .OldAnnotation("Npgsql:Enum:grade", "a,b,c,d,f,s,sh,x,xh")
+                .OldAnnotation("Npgsql:Enum:mode", "fruits,mania,osu,taiko")
+                .OldAnnotation("Npgsql:Enum:scan_event_type", "main_seeding_finished,main_seeding_started,rescan_finished,rescan_started,secondary_seeding_finished,secondary_seeding_started,user_check_finished,user_check_started,user_scan_finished,user_scan_started")
+                .OldAnnotation("Npgsql:Enum:score_source", "leaderboard_scan,score_fetcher");
+        }
+    }
+}
